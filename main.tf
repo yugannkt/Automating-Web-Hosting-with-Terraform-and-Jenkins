63c534dd4b7ca1bdc92c86de8d932e1a206c5f27
yugannkt <166296076+yugannkt@users.noreply.github.com>
2024-08-13 07:09:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,14 @@ resource "aws_instance" "jenkins_server" {
       "sudo apt update -y",
       "sudo apt upgrade -y",
       "sudo apt install openjdk-11-jdk -y",
+      "sudo apt-get install -y apache2",
       "sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key",
       "echo 'deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/' | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
       "sudo apt-get update -y",
       "sudo apt-get install jenkins -y",
       "sudo apt-get install systemd -y",
+      "sudo systemctl start apache2",
+      "sudo systemctl start apache2",
       "sudo systemctl start jenkins",
       "sudo systemctl enable jenkins"
     ]
